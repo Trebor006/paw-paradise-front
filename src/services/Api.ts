fad2eb44f7c6f94026006e929a1b3a5ed5c56279
Robert Cabrera <clrobert.06@gmail.com>
2025-04-19 18:02:49 -0400

    feat(PPP-77): update ci.yml

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,15 +1,1 @@
-import dotenv from 'dotenv';
-
-// Helper function to safely get environment variables
-function getEnv(variable: string): string {
-    const value = dotenv.config().parsed?.[variable];
-    if (!value) {
-        console.error(`Environment variable ${variable} is not defined or empty`);
-        throw new Error(`Environment variable ${variable} is not defined`);
-    }
-    console.log(`Environment variable ${variable} loaded with value: ${value}`);
-    return value;
-}
-
-// Use getEnv to retrieve the API URL
-export const baseUrl = getEnv('API_URL');
+export const baseUrl = import.meta.env.VITE_API_URL;
